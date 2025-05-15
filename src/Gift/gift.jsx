e6120ea3af39e1306cd94a285a18f667c26297c5
habibamohamed4 <61379163+habibamohamed4@users.noreply.github.com>
2025-05-15 18:55:18 +0300

    Update gift.jsx

--- src/Gift/gift.jsx
+++ src/Gift/gift.jsx
@@ -4,7 +4,7 @@ export default function GiftPage() {
   return (
     <div className="video-container">
       <video controls autoPlay loop width="100%">
-        <source src="/Gift/video1.mp4" type="video/mp4" />
+        <source src="./Gift/video1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
